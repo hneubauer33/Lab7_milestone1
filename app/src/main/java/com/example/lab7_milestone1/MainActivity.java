16c966ea631cbebe1dfe235b9df5cbc7498237c7
hneubauer33 <58747903+hneubauer33@users.noreply.github.com>
2021-11-15 14:18:06 -0600

    Finished Lab 7

--- app/src/main/java/com/example/lab7_milestone1/MainActivity.java
+++ app/src/main/java/com/example/lab7_milestone1/MainActivity.java
@@ -3,15 +3,18 @@ package com.example.lab7_milestone1;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
-
 import android.app.Notification;
-import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+
 import static com.example.lab7_milestone1.App.CHANNEL_1_ID;
 import static com.example.lab7_milestone1.App.CHANNEL_2_ID;
+
 
 public class MainActivity extends AppCompatActivity {
     private NotificationManagerCompat notificationManager;
@@ -24,15 +27,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         notificationManager = NotificationManagerCompat.from(this);
-        editTextTitle = findViewById(R.id.editTextTextPersonName);
-        editTextTitle = findViewById(R.id.editTextTextPersonName3);
+        editTextTitle = findViewById(R.id.title);
+        editTextMessage = findViewById(R.id.message);
     }
-    public void sendOnChannel1(View v){
+
+    public void sendOnChannel1(View v) {
+        Log.i("send", "send on channel 1");
         String title = editTextTitle.getText().toString();
         String message = editTextMessage.getText().toString();
-        System.out.println(title + "this is a print");
-        System.out.println(message + "This is a print");
 
+        Intent activityIntent = new Intent(this, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, activityIntent, 0);
+
+        Intent broadcastIntent = new Intent(this, NotificationReceiver.class);
+        broadcastIntent.putExtra("toastMessage", message);
+        PendingIntent actionIntent = PendingIntent.getBroadcast(this, 0, broadcastIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
         Notification notification = new NotificationCompat.Builder(this, CHANNEL_1_ID)
                 .setSmallIcon(R.drawable.ic_chat_black_24dp)
@@ -41,26 +50,24 @@ public class MainActivity extends AppCompatActivity {
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setCategory(NotificationCompat.CATEGORY_MESSAGE)
                 .setColor(Color.BLUE)
+                .setContentIntent(contentIntent)
                 .setAutoCancel(true)
                 .setOnlyAlertOnce(true)
+                .addAction(R.mipmap.ic_launcher, "Toast", actionIntent)
                 .build();
         notificationManager.notify(1, notification);
-
     }
-    public void sendOnChannel2(View v){
+
+    public void sendOnChannel2(View v) {
         String title = editTextTitle.getText().toString();
         String message = editTextMessage.getText().toString();
-        System.out.println(title + "this is a print");
-        System.out.println(message + "This is a print");
-
-        Notification notification2 = new NotificationCompat.Builder(this, CHANNEL_2_ID)
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_2_ID)
                 .setSmallIcon(R.drawable.ic_baseline_send_24)
                 .setContentTitle(title)
                 .setContentText(message)
                 .setPriority(NotificationCompat.PRIORITY_LOW)
-                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
                 .build();
-        notificationManager.notify(2, notification2);
 
+        notificationManager.notify(2, notification);
     }
 }
